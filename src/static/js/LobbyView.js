5aea64b45236c068be87bb4413dc1c9864b8df17
Jan Cwik <halpon6@gmail.com>
2019-05-08 18:55:37 +0200

    Changed LobbyView.js not much

--- src/static/js/LobbyView.js
+++ src/static/js/LobbyView.js
@@ -10,23 +10,9 @@ class LobbyView {
             .append($("<p>").attr("id", "name1").html("Civilization VII"))
             .append($("<p>").attr("id", "name2").addClass("name").html("Lista graczy"))
             .append($("<p>").attr("id", "name3").addClass("name").html("Menu"))
-        //
         this._createListOfPlayers();
         this._createInterialMenu();
         main.append(this.menu)
-        $(".name")
-            .css("color", "gold")
-            .css("font-size", "50px")
-            .css("position", "absolute")
-            .css("top", "17%")
-            .css("font-family", "fantasy")
-        $(".menu_buttons")
-            .css("font-family", "fantasy")
-            .css("color", "gold")
-            .css("font-size", "40px")
-            .css("position", "absolute")
-            .css("background-color", "rgba(192, 192, 192, 0.6)")
-        //
     }
 
     _createListOfPlayers() {
@@ -47,8 +33,6 @@ class LobbyView {
             .append(this.newGameButton)
             .append(this.savedGameButton)
         this.menu.append(interialMenu);
-        //
-        //
     }
 
     _addingPlayers() {
@@ -57,22 +41,19 @@ class LobbyView {
             { id: 2, nick: "MisterCodePL" }
         ]
         for (var i = 0; i < this.players.length; i++) {
+            this.nextPlayerId = ("<div>")
+                .addClass("playerId")
+                .html("Lp. " + this.players[i].id)
+            this.nextPlayerNick = ("<div>")
+                .addClass("playerNick")
+                .html("Nick: " + this.players[i].nick)
+
+            var nextPlayer = $("<div>").addClass("nextPlayer")
+                .append(this.nextPlayerId)
+                .append(this.nextPlayerNick)
+
             this.listOfPlayers
-                .append("<div id='player_" + (i + 1) + "'></div>")
-            $("#player_" + (i + 1))
-                .css("color", "silver")
-                .append("<p id='player_lp_" + (i + 1) + "'>Lp.: " + this.players[i].id + "</p>")
-                .append("<p id='player_nick_" + (i + 1) + "'>Nick: " + this.players[i].nick + "</p>")
-            $("#player_lp_" + (i + 1))
-                .css("position", "absolute")
-                .css("top", ((i + 1) * 5) + "%")
-                .css("right", "5%")
-                .css("font-family", "Courier New")
-            $("#player_nick_" + (i + 1))
-                .css("position", "absolute")
-                .css("top", ((i + 1) * 5) + "%")
-                .css("left", "5%")
-                .css("font-family", "Courier New")
+                .append(nextPlayer)
         }
     }
 }
