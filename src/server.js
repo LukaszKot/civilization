0dbcb16f5f2b0dcffb26033a3b5c8cc039237731
Łukasz Kot <lukaszkot03@gmail.com>
2019-05-09 11:00:22 +0200

    Added route / to index.html.

--- src/server.js
+++ src/server.js
@@ -4,9 +4,12 @@ var app = express()
 const PORT = 3000;
 
 app.use(express.static('static'))
+var path = require('path')
 
-
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/static/index.html"))
+})
 
 app.listen(PORT, function () {
-    console.log("start serwera na porcie" + PORT)
+    console.log("start serwera na porcie " + PORT)
 })
