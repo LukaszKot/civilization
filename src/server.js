f3d6e8f3fe8aed08e88aafc40dd0c5452627bdca
Łukasz Kot <lukaszkot03@gmail.com>
2019-05-09 11:19:09 +0200

    Added endpoint for getting lobbies.

--- src/server.js
+++ src/server.js
@@ -10,6 +10,10 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + "/static/index.html"))
 })
 
+app.get("/api/lobbies", function (req, res) {
+
+})
+
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
