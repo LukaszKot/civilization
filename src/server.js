80fac1b375f9db39b7005e63ac17394fe7e997dd
Łukasz Kot <lukaszkot03@gmail.com>
2019-05-09 11:40:30 +0200

    Added getting all from LobbiesRepository.

--- src/server.js
+++ src/server.js
@@ -5,13 +5,37 @@ const PORT = 3000;
 
 app.use(express.static('static'))
 var path = require('path')
+var Datastore = require('nedb')
+
+class LobbiesRepository {
+    constructor() {
+        this._collection = new Datastore({
+            filename: 'db/lobbies.db',
+            autoload: true
+        })
+    }
+
+    getAll() {
+        return new Promise((accept, reject) => {
+            this._collection.find({}, (err, docs) => {
+                if (err) reject(err);
+                accept(docs)
+            })
+        })
+    }
+}
+
+var lobbiesRepository = new LobbiesRepository();
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + "/static/index.html"))
 })
 
 app.get("/api/lobbies", function (req, res) {
-
+    lobbiesRepository.getAll()
+        .then(x => {
+            res.send(x);
+        })
 })
 
 app.listen(PORT, function () {
