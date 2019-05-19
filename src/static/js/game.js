16292a62870cf4d2ed13c56f34abf24e31ee2492
Łukasz Kot <lukaszkot03@gmail.com>
2019-05-19 21:35:23 +0200

    Added default three.js environment.

--- src/static/js/game.js
+++ src/static/js/game.js
@@ -1,3 +1,34 @@
 var net;
+var scene;
+var camera;
+var renderer;
+var windowObject = $(window);
 $(document).ready(function () {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+        45,
+        windowObject.width() / windowObject.height(),
+        0.1,
+        10000
+    );
+    camera.position.set(0, 250, 200);
+    camera.lookAt(scene.position);
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0xffffff);
+    renderer.setSize(windowObject.width(), windowObject.height())
+    $("#root").append(renderer.domElement);
+
+    var axes = new THREE.AxesHelper(1000)
+    scene.add(axes)
+
+
+
+
+
+
+    function render() {
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    }
+    render();
 })
