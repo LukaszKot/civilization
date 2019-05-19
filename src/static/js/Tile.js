1449e48b468716c3ef2144de193a1aa9d9ca132a
Łukasz Kot <lukaszkot03@gmail.com>
2019-05-19 21:48:00 +0200

    Started creating geometry.

--- src/static/js/Tile.js
+++ src/static/js/Tile.js
@@ -1,5 +1,6 @@
 class Tile extends THREE.Mesh {
     constructor() {
-
+        var geometry = new THREE.CylinderGeometry(5, 5, 1, 6);
+        var material = new THREE.MeshBasicMaterial(5, 5)
     }
 }
