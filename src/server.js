6413539755890edd109dce9610718b34966197fe
Łukasz Kot <lukaszkot03@gmail.com>
2019-06-01 20:07:18 +0200

    Moved LobbiesRepository to seperate file.

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ const PORT = 3000;
 app.use(express.static('static'))
 app.use(express.json())
 var path = require('path')
-var Datastore = require('nedb')
 
 var server = app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
@@ -17,70 +16,8 @@ var io = require('socket.io')(server);
 var sockets = []
 
 var SavesRepository = require("./repositories/SavesRepository.js").SavesRepository
+var LobbiesRepository = require("./repositories/LobbiesRepository.js").LobbiesRepository
 
-class LobbiesRepository {
-    constructor() {
-        this._collection = new Datastore({
-            filename: 'db/lobbies.db',
-            autoload: true
-        })
-    }
-
-    getAll() {
-        return new Promise((accept, reject) => {
-            this._collection.find({}, (err, docs) => {
-                if (err) reject(err);
-                accept(docs)
-            })
-        })
-    }
-
-    getSingle(name) {
-        return new Promise((accept, reject) => {
-            this._collection.findOne({ name: name }, (err, docs) => {
-                if (err) reject(err);
-                accept(docs)
-            })
-        })
-    }
-
-    insert(name, players = [], save = null) {
-        return new Promise((accept, reject) => {
-            var doc = {
-                name: name,
-                players: players,
-                save: null
-            }
-            this._collection.insert(doc, (err, newDoc) => {
-                if (err) reject(err);
-                accept(newDoc);
-            })
-        })
-    }
-
-    update(lobby) {
-        return new Promise((accept, reject) => {
-            this._collection.update({ _id: lobby._id }, { $set: lobby }, {}, (err, numUpdated) => {
-                if (err) reject(err)
-                accept(lobby)
-            })
-        })
-    }
-
-    delete(id) {
-        return new Promise((accept, reject) => {
-            this._collection.remove({ _id: id }, {}, (err, numRemoved) => {
-                if (err) reject(err)
-                accept(numRemoved)
-            })
-        })
-    }
-
-    drop() {
-        this._collection.remove({}, { multi: true }, function (err, numRemoved) {
-        });
-    }
-}
 
 class SocketRepository {
     constructor() {
