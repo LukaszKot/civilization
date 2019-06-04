3a60d41c7dd45f372bca31dfe2d3e031165cdfc3
Łukasz Kot <lukaszkot03@gmail.com>
2019-06-04 20:18:35 +0200

    Changed positioning divs from iterator variables to tile data.

--- src/static/js/game/Map.js
+++ src/static/js/game/Map.js
@@ -4,18 +4,18 @@ class Map {
         this.game = game;
         var tileRadius = Settings.tileRadius;
         this.container = new THREE.Object3D();
-        for (var i = 0; i < this.game.map.size.width; i++) {
-            for (var j = 0; j < this.game.map.size.height; j++) {
-                var tile = new Tile();
-                if (j % 2 == 1) {
-                    tile.position.set(tileRadius * Math.sqrt(3) * i + tileRadius * Math.sqrt(3) / 2, 0, 2 * j * tileRadius - tileRadius / 2 * j);
-                }
-                else {
-                    tile.position.set(tileRadius * Math.sqrt(3) * i, 0, 2 * j * tileRadius - tileRadius / 2 * j);
-                }
 
-                this.container.add(tile)
+        for (var i = 0; i < this.game.map.tiles.length; i++) {
+            var tileData = this.game.map.tiles[i];
+            console.log(tileData)
+            var tile = new Tile();
+            if (tileData.position.z % 2 == 1) {
+                tile.position.set(tileRadius * Math.sqrt(3) * tileData.position.x + tileRadius * Math.sqrt(3) / 2, 0, 2 * tileData.position.z * tileRadius - tileRadius / 2 * tileData.position.z);
             }
+            else {
+                tile.position.set(tileRadius * Math.sqrt(3) * tileData.position.x, 0, 2 * tileData.position.z * tileRadius - tileRadius / 2 * tileData.position.z);
+            }
+            this.container.add(tile)
         }
         cameraController.setEndsOfMap(tileRadius * Math.sqrt(3) * (this.game.map.size.width - 1), 2 * (this.game.map.size.height - 1) * tileRadius - tileRadius / 2 * (this.game.map.size.height - 1))
     }
