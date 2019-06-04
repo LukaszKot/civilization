057b1532ca66ad8b99226f75a7144948ca9f7977
Łukasz Kot <lukaszkot03@gmail.com>
2019-06-04 23:01:42 +0200

    Deleted axis helpers.

--- src/static/js/game/game.js
+++ src/static/js/game/game.js
@@ -26,8 +26,6 @@ $(document).ready(async function () {
     renderer.setSize(windowObject.width(), windowObject.height())
     $("#root").append(renderer.domElement);
 
-    var axes = new THREE.AxesHelper(1000)
-    scene.add(axes)
     var map = await Map.create();
     scene.add(map.container)
 
